7890409c99a2855af85f1e7e8ea6f2f9093f90fd
elliotsackman <elliotsackman@gmail.com>
2025-02-05 13:55:33 -0500

    Updated user create trigger to include metadata (first, last, username) if exists at user create. Updated seed data to take this into account, and add encrypted passwords to allow account login for seeded users.

---
+++ supabase/migrations/20250205184647_add_user_metadata_to_profile_create_trigger.sql
@@ -0,0 +1,14 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$BEGIN
+  INSERT INTO public.profiles (id, email, first_name, last_name, display_name)
+  VALUES (NEW.id, NEW.email, NEW.raw_user_meta_data ->> 'first_name', NEW.raw_user_meta_data ->> 'last_name', NEW.raw_user_meta_data ->> 'username');
+  RETURN NEW;
+END;$function$
+;
+
+
--- supabase/seed/users_and_profiles.sql
+++ supabase/seed/users_and_profiles.sql
@@ -1,38 +1,14 @@
 INSERT INTO auth.users
-    (id, email)
+    (id, email, encrypted_password, raw_user_meta_data)
 VALUES
-    (gen_random_uuid(), 'seed_test1@gmail.com'),
-    (gen_random_uuid(), 'seed_test2@gmail.com'),
-    (gen_random_uuid(), 'seed_test3@gmail.com'),
-    (gen_random_uuid(), 'seed_test4@gmail.com'),
-    (gen_random_uuid(), 'seed_test5@gmail.com');
+    (gen_random_uuid(), 'seed_test1@gmail.com', '$2a$10$xpJ43DNgHiW.eOVjfMr89u2OJZuMAfPktkfXlxMNufn.TYljyxwZ6', '{"first_name":"seed", "last_name": "one", "username":"seed one"}'),
+    (gen_random_uuid(), 'seed_test2@gmail.com', '$2a$10$xpJ43DNgHiW.eOVjfMr89u2OJZuMAfPktkfXlxMNufn.TYljyxwZ6', '{"first_name":"seed", "last_name": "two", "username":"seed two"}'),
+    (gen_random_uuid(), 'seed_test3@gmail.com', '$2a$10$xpJ43DNgHiW.eOVjfMr89u2OJZuMAfPktkfXlxMNufn.TYljyxwZ6', '{"first_name":"seed", "last_name": "three", "username":"seed three"}'),
+    (gen_random_uuid(), 'seed_test4@gmail.com', '$2a$10$xpJ43DNgHiW.eOVjfMr89u2OJZuMAfPktkfXlxMNufn.TYljyxwZ6', '{"first_name":"seed", "last_name": "four", "username":"seed four"}'),
+    (gen_random_uuid(), 'seed_test5@gmail.com', '$2a$10$xpJ43DNgHiW.eOVjfMr89u2OJZuMAfPktkfXlxMNufn.TYljyxwZ6', '{"first_name":"seed", "last_name": "five", "username":"seed five"}');
 
 UPDATE public.profiles
 SET 
-    display_name = CASE email  
-        WHEN 'seed_test1@gmail.com' THEN 'seed_1'
-        WHEN 'seed_test2@gmail.com' THEN 'seed_2'
-        WHEN 'seed_test3@gmail.com' THEN 'seed_3'
-        WHEN 'seed_test4@gmail.com' THEN 'seed_4'
-        WHEN 'seed_test5@gmail.com' THEN 'seed_5'
-        ELSE NULL
-    END,
-    first_name = CASE email
-        WHEN 'seed_test1@gmail.com' THEN 'John'
-        WHEN 'seed_test2@gmail.com' THEN 'Jane'
-        WHEN 'seed_test3@gmail.com' THEN 'Mike'
-        WHEN 'seed_test4@gmail.com' THEN 'Emily'
-        WHEN 'seed_test5@gmail.com' THEN 'Chris'
-        ELSE NULL
-    END,
-    last_name = CASE email
-        WHEN 'seed_test1@gmail.com' THEN 'Doe'
-        WHEN 'seed_test2@gmail.com' THEN 'Smith'
-        WHEN 'seed_test3@gmail.com' THEN 'Johnson'
-        WHEN 'seed_test4@gmail.com' THEN 'Davis'
-        WHEN 'seed_test5@gmail.com' THEN 'Brown'
-        ELSE NULL
-    END,
     birthday = CASE email
         WHEN 'seed_test1@gmail.com' THEN '1990-01-01'::DATE
         WHEN 'seed_test2@gmail.com' THEN '1992-05-15'::DATE
