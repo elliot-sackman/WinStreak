9dc8f9b892049bc357a59ac63cc16388259c0d30
elliotsackman <elliotsackman@gmail.com>
2025-03-03 11:11:15 -0500

    Added api details for NBA teams.

--- supabase/seed/leagues.sql
+++ supabase/seed/leagues.sql
@@ -1,6 +1,6 @@
 INSERT INTO public.leagues 
-    (league_name, league_abbreviation, sport)  
+    (league_name, league_abbreviation, sport, league_api_id)  
 VALUES  
-    ('Major League Baseball', 'MLB', 'Baseball'),
-    ('National Basketball Association', 'NBA', 'Basketball'),  
-    ('National Football League', 'NFL' , 'Football');
+    ('Major League Baseball', 'MLB', 'Baseball', 1),
+    ('National Basketball Association', 'NBA', 'Basketball', 12),  
+    ('National Football League', 'NFL' , 'Football', NULL);
