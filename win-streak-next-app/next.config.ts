f984def2a8459759af18ea1aeba97f35ab59e6b9
elliotsackman <elliotsackman@gmail.com>
2025-03-19 16:00:27 -0400

    Update allowed domains for loading from remote to include the supabase production project.

--- win-streak-next-app/next.config.ts
+++ win-streak-next-app/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["127.0.0.1", "supabase.co"],
+    domains: ["127.0.0.1", "dfpzvgfqsjdgvpaarxwc.supabase.co"],
   },
 };
 
