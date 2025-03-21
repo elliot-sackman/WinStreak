a35d1c06ca60b0d97c87b9740eb724c2c6599a4c
elliotsackman <elliotsackman@gmail.com>
2025-03-21 15:01:04 -0400

    Updated update-games-picks-entries db function to include new columns on picks and entries table.

---
+++ supabase/migrations/20250321185525_add_new_columns_to_update_g_p_e_function.sql
@@ -0,0 +1,57 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.update_games_picks_entries(updategames jsonb[], updatepicks jsonb[], updateentries jsonb[])
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$DECLARE
+  record JSONB;
+BEGIN
+  -- Update games table
+  FOREACH record IN ARRAY updateGames
+  LOOP
+    UPDATE games
+    SET 
+      home_team_win = (record->>'home_team_win')::BOOLEAN,
+      away_team_win = (record->>'away_team_win')::BOOLEAN,
+      status = (record->>'status')::game_status
+    WHERE game_id = (record->>'game_id')::INT;
+  END LOOP;
+
+  -- Update picks table
+  FOREACH record IN ARRAY updatePicks
+  LOOP
+    UPDATE picks
+    SET 
+      home_team_win = (record->>'home_team_win')::BOOLEAN,
+      away_team_win = (record->>'away_team_win')::BOOLEAN,
+      pick_status = (record->>'pick_status')::picks_pick_status,
+      pick_resolution_datetime = (record->>'pick_resolution_datetime')::TIMESTAMPTZ
+    WHERE pick_id = (record->>'pick_id')::INT;
+  END LOOP;
+
+  -- Update entries table
+  FOREACH record IN ARRAY updateEntries
+  LOOP
+    UPDATE entries
+    SET 
+      current_streak = (record->>'current_streak')::INT,
+      is_complete = (record->>'is_complete')::BOOLEAN,
+      first_incorrect_pick_id = 
+        CASE 
+          WHEN record->>'first_incorrect_pick_id' IS NOT NULL
+          THEN (record->>'first_incorrect_pick_id')::INT 
+          ELSE first_incorrect_pick_id 
+        END,
+      entry_completion_datetime = 
+        CASE 
+          WHEN record->>'entry_completion_datetime' IS NOT NULL
+          THEN (record->>'entry_completion_datetime')::TIMESTAMPTZ 
+          ELSE entry_completion_datetime 
+        END
+    WHERE entry_id = (record->>'entry_id')::INT;
+  END LOOP;
+
+END;$function$
+;
+
+
