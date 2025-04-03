e60123228f620c69d4f0d8799c2569c1f41f3388
elliotsackman <elliotsackman@gmail.com>
2025-04-03 09:53:15 -0400

    Fixed bug in handlePicks with new pick_status_type.

---
+++ supabase/migrations/20250403133527_update_handle_picks_with_pick_status_type.sql
@@ -0,0 +1,44 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_picks(insert_data jsonb[], update_data jsonb[], delete_data integer[])
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$DECLARE
+  record JSONB;
+BEGIN
+  -- Insert Data
+  FOREACH record IN ARRAY insert_data
+  LOOP
+    INSERT INTO picks (contest_id, entry_id, user_id, pick_type, value, game_id, pick_status, pick_datetime)
+    VALUES (
+      (record->>'contest_id')::INT,
+      (record->>'entry_id')::INT,
+      (record->>'user_id')::UUID,
+      record->>'pick_type',
+      (record->>'value')::INT,
+      (record->>'game_id')::INT,
+      (record->>'pick_status')::pick_status_type,
+      NOW()
+    );
+  END LOOP;
+
+  -- Update Data
+  FOREACH record IN ARRAY update_data
+  LOOP
+    UPDATE picks
+    SET value = (record->>'value')::INT,
+        pick_datetime = NOW()
+    WHERE pick_id = (record->>'pick_id')::INT;
+  END LOOP;
+
+  -- Delete Data
+  FOREACH record IN ARRAY delete_data
+  LOOP
+    DELETE FROM picks
+    WHERE pick_id = record::INT;
+  END LOOP;
+
+END;$function$
+;
+
+
