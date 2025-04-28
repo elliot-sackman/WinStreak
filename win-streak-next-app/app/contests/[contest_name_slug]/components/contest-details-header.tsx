bf3647fe420a46dd971131c67548c4d295e3897c
elliotsackman <elliotsackman@gmail.com>
2025-04-28 16:42:36 -0400

    Added background for contest details header for MLB only.

--- win-streak-next-app/app/contests/[contest_name_slug]/components/contest-details-header.tsx
+++ win-streak-next-app/app/contests/[contest_name_slug]/components/contest-details-header.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { AllContestGamesPicksEntries } from "@/lib/types";
+import mlbContestBackground from "@/app/static-images/mlb-contest-background.jpg";
 
 interface ContestDetailsHeaderProps {
   contestData: AllContestGamesPicksEntries;
@@ -19,33 +20,47 @@ export default function ContestDetailsHeader({
       : null;
 
   return (
-    <div>
-      <h1 className="text-2xl font-bold">{contest.contest_name}</h1>
-      {contest.sponsor_id && (
-        <div className="flex flex-col items-center my-4">
-          <span className="text-neutral-500 mb-2">Sponsored By:</span>
-          <Link href={contest.sponsor_site_url!}>
-            <Image
-              src={contest.sponsor_logo_url!}
-              alt="Sponsor Logo"
-              width={200}
-              height={100}
-              style={{ width: "auto" }}
-            />
-          </Link>
-        </div>
+    <div className="relative w-full max-w-sm text-white">
+      {contest.sport === "Baseball" && (
+        <div
+          className="absolute inset-0 bg-cover bg-center opacity-30"
+          style={{
+            backgroundImage: `url(${mlbContestBackground.src})`,
+          }}
+        ></div>
       )}
-      {activeEntry ? (
-        <div className="flex flex-row items-center my-4 justify-between w-[90%] max-w-sm">
-          <span className="text-2xl italic">Your Streak</span>
-          <div className="flex-grow h-[1px] rounded-r-full bg-gradient-to-r from-neutral-800 to-green-800 mx-4"></div>
-          <div className="text-2xl">
-            🔥<span className="italic">{activeEntry.current_streak}</span>
+
+      {/* Content */}
+      <div className="relative p-8">
+        <h1 className="text-2xl font-bold">{contest.contest_name}</h1>
+        {contest.sponsor_id && (
+          <div className="flex flex-col items-center my-4">
+            <span className="text-white mb-2">Sponsored By:</span>
+            <Link href={contest.sponsor_site_url!}>
+              <Image
+                src={contest.sponsor_logo_url!}
+                alt="Sponsor Logo"
+                width={200}
+                height={100}
+                style={{ width: "auto" }}
+              />
+            </Link>
           </div>
-        </div>
-      ) : (
-        <div>Enter the contest to start your streak 🔥</div>
-      )}
+        )}
+        {activeEntry ? (
+          <div className="flex flex-row items-center my-4 justify-between w-[90%] max-w-sm">
+            <span className="text-2xl italic">Your Streak</span>
+            <div className="flex-grow h-[1px] rounded-r-full bg-gradient-to-r from-neutral-800 to-green-800 mx-4"></div>
+            <div className="text-2xl">
+              🔥<span className="italic">{activeEntry.current_streak}</span>
+            </div>
+          </div>
+        ) : (
+          contest.contest_status === "in_progress" && (
+            <div>Enter the contest to start your streak 🔥</div>
+          )
+        )}
+      </div>
     </div>
   );
 }
